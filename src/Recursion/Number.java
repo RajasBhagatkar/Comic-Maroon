0dcd321174b3a483fa66fda7dc5198215d34ab1f
RajasBhagatkar <rajasbhagatkar@gmail.com>
2022-09-09 14:10:05 +0530

    starting Recursion day 9

---
+++ src/Recursion/Number.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Number {
+    public static void main(String[] args) {
+        // wirte a function that takes in a number and prints it
+        // print first 5 numbers: 1 2 3 4 5
+        print(5);
+    }
+    static void print(int n){
+        
+    }
+}
---
+++ src/Recursion/test.java
@@ -0,0 +1,27 @@
+package Recursion;
+// Nikal na Important hai
+
+public class test {
+  public static void main(String[] args) {
+    message1();
+  }
+  static void message1(){
+    System.out.println("Hello there");
+    message2();
+  }
+  static void message2(){
+    System.out.println("Hello there");
+    message3();
+  }
+  static void   message3(){
+    System.out.println("Hello there");
+    message4();
+  }
+  static void message4(){
+    System.out.println("Hello there");
+    message5();
+  }
+  static void message5(){
+    System.out.println("Hello there");
+  }
+}
